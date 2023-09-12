f5710a8b4b09928f2a84b780caec0f667ad465d1
Karoline Moraes <125392501+omkarol21@users.noreply.github.com>
2023-09-12 15:54:29 -0300

    Add files via upload

---
+++ EstacaoLanche/assets/js/script.js
@@ -0,0 +1,14 @@
+
+function exibirCombo() {
+   document.getElementById('Lan1').style.display = 'none'
+   document.getElementById('Lan2').style.display = 'none'
+   document.getElementById('Com').style.display = 'flex'
+}
+
+function exibirLanche() {
+    document.getElementById('Lan1').style.display = 'flex'
+    document.getElementById('Lan2').style.display = 'flex'
+    document.getElementById('Com').style.display = 'none'
+
+ 
+ }
